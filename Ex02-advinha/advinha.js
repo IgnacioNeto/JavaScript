c5b65a2acd3d92062eebf034d5f813736c103925
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-04-29 12:04:20 -0300

    Adição de exercício Advinha
    Adição de exercício Advinha

---
+++ Ex02-advinha/advinha.js
@@ -0,0 +1,21 @@
+let formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let numeroEscolhido = parseInt(Math.random() * 11);
+    let palpiteUsuario = parseInt(document.getElementById('palpite').value);
+    let resultado = document.getElementById('resultado');
+
+    if (palpiteUsuario == numeroEscolhido) {
+        resultado.innerHTML = "Você acertou!";
+
+    } else if (palpiteUsuario < 0 || palpiteUsuario > 10) {
+        resultado.innerHTML = "Digite um número entre 0 e 10!";
+
+    } else {
+        resultado.innerHTML = "Você errou...";
+    };
+
+});
+
