8f872572e8f5d5496c51447ce52cbae65b2c5d34
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-06-07 12:00:53 -0300

    Revisão p3 em js
    Revisão p3 em js

---
+++ revisao-js/revisao-p3.js
@@ -0,0 +1,23 @@
+let fomulario = document.querySelector('form');
+let conteudo = document.getElementById('conteudo');
+
+fomulario.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let nasc = document.getElementById("nasc").value;
+
+    var data = new Date();
+    var ano = data.getFullYear();
+
+    let idade = ano - nasc;
+
+    if(idade >=18) {
+        conteudo.innerHTML = `<p>${nome} tem ${idade} anos e já pode dirigir.`
+    
+    } else {
+        conteudo.innerHTML = `<p>${nome} só poderá dirigir quando fizer 18 anos.`
+
+    }
+});
